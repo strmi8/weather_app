fdc21bf42ffe1f759d10faeba4d1cb6b1b99d2a5
Stjepan Strmečki <stjepan.strmecki01@gmail.com>
2023-01-02 14:55:37 +0100

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '76cfd129fdmsha0ab9a13aea04c5p16bc34jsndcfc1cf7185d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d93c25b6eab2e22c57e49c775b26bb7b";
